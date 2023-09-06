9f798e00185c7660d59a7223d18bfe45f28acbde
Happy Gina <254355496@qq.com>
2023-09-06 22:29:16 +0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,103 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Author: Gina
+ * @Date: 2023-09-06 15:03:15
+ * @LastEditors: Gina
+ * @LastEditTime: 2023-09-06 22:16:01
+ */
+
+
+interface Week {
+    name: string
+    value: string[]
+}
+interface WeekLans {
+    [attr:string]: string[]
+}
+class FormatSetting {
+    weekLans:WeekLans = {
+        zh: Array.from('一二三四五六日').map(v => '周' + v),
+        en: ['Mon.','Tues.','Wed.','Thur.','Fri.','Sat.','Sun.']
+    }
+    constructor(...lans:Week[]) {
+        if(lans) this.addWeekLans(...lans);
+        return this
+    }
+    format = (date: Date, formatStr:string, weekLan:string = 'zh') => {
+        const currentDate = new Date(date)
+        if(isNaN(currentDate.getDay())) return date;
+        const formater = formatStr.replace(/\s+/g, ' ')
+        const __Date = {
+            year: '',
+            month: '',
+            day: '',
+            week: '',
+            hour: '',
+            minute: '',
+            second: '',
+        }
+        formater.split(' ')
+            .forEach(str => {
+                const regEx = /[@\-.:年月日时分秒周]/g
+                str
+                    .split(regEx)
+                    .forEach((type) => Object.assign(__Date, this.setAttr(type, currentDate)))
+            })
+        const result = formatStr
+            .replace(/m+/g, __Date.minute)
+            .replace(/M+/g, __Date.month)
+            .toLowerCase()
+            .replace(/y+/g, __Date.year)
+            .replace(/d+/g, __Date.day)
+            .replace(/w+/g, this.weekLans[weekLan as keyof typeof this.weekLans ][Number(__Date.week) - 1])
+            .replace(/h+/g, __Date.hour)
+            .replace(/s+/g, __Date.second)
+        
+        return result
+    }
+
+    setAttr = (type:string, date:Date) => {
+        const str = type.toLowerCase()
+        if(/^y+$/g.test(str)) {//年
+            const l = type.length
+            const y = date.getFullYear().toString()
+            return {year: y.slice(l > y.length - 1 ? 0 : l)}
+        } 
+        if(/^M+$/g.test(type)) {//月
+            return {month: this.padStart(date.getMonth() + 1, type.length)}
+        } 
+        
+        if (/^d+$/g.test(str)) {//日
+            return {day: this.padStart(date.getDate(), type.length)}
+        }
+        
+        if (/^w+$/g.test(type)) {//星期
+            return {week: date.getDay().toString()}
+        }
+        
+        if (/^h+$/g.test(str)) {//时
+            return {hour: this.padStart(date.getHours(), type.length)}
+        }
+        
+        if (/^m+$/g.test(type)) {//分
+            return {minute: this.padStart(date.getMinutes(), type.length)}
+        }
+        
+        if (/^s+$/g.test(str)) {//秒
+            return {second: this.padStart(date.getSeconds(), type.length)}
+        } 
+        return {}
+    }
+    
+    padStart = (temp: string | number, length:number) => {
+        const result = temp.toString()
+        return result.padStart(length, '0')
+    } 
+    addWeekLans = (...lans:Week[]) => {
+        lans.forEach((v:Week) => {
+            this.weekLans[v.name as keyof typeof this.addWeekLans] = v.value
+        })
+    }
+}
+export default FormatSetting
